c7d03d0b29502e8626d49e644ea4950b7ea5c3b2
gabriel martins <gabrielmartinsid@gmail.com>
2019-09-18 17:06:58 -0300

    - Adding player health bar

---
+++ Assets/Scripts/Client/PlayerHealthComponent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using MLAPI;
+using MLAPI.NetworkedVar;
+
+public class PlayerHealthComponent : NetworkedBehaviour
+{
+    [SerializeField] PlayerHealthHUD hud;
+    public static float MaxHealth = 5f;
+    public NetworkedVar<float> Health = new NetworkedVar<float>(MaxHealth);
+}
---
+++ Assets/Scripts/Client/PlayerHealthHUD.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealthHUD : MonoBehaviour
+{
+    [SerializeField] Slider healthBar;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
